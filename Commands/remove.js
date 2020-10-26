d64b917e4fa273bad45315c3d7000577d95900bc
BuildTools <randysim9@gmail.com>
2020-10-25 23:03:38 -0400

    added remove command

---
+++ Commands/remove.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+const Fs = require("fs");
+
+module.exports = {
+    names: ["remove"],
+    execute(Env) {
+        const message = Env.message;
+        const args = Env.args;
+
+        if (message.author.id != "325447731676184576") {
+            const ErrorEmbed = new Discord.MessageEmbed()
+                .setTitle("**Insufficient Permission**")
+                .setDescription("No");
+            return message.channel.send(ErrorEmbed);
+        }
+
+        if (!["greeting", "goodbye", "insult", "compliment"].includes(args[1])) {
+            const ErrorEmbed = new Discord.MessageEmbed()
+                .setTitle("**ERROR**")
+                .setDescription("Specify category [greeting, goodbye, insult, compliment]");
+            return message.channel.send(ErrorEmbed);
+        }
+
+        if (!["pattern", "response"].includes(args[2])) {
+            const ErrorEmbed = new Discord.MessageEmbed()
+                .setTitle("**ERROR**")
+                .setDescription("Specify type [pattern, response]");
+            return message.channel.send(ErrorEmbed);
+        }
+
+        var sentence = args.slice(3).join(" ");
+        if (!sentence) {
+            const ErrorEmbed = new Discord.MessageEmbed()
+                .setTitle("**ERROR**")
+                .setDescription("Specify phrase to delete from dataset");
+            return message.channel.send(ErrorEmbed);
+        }
+
+        var Dataset = JSON.parse(Fs.readFileSync("./Convo/Dataset.json"));
+        Dataset[args[1]][args[2] + "s"] = Dataset[args[1]][args[2] + "s"].filter(e => e != sentence);
+        Fs.writeFileSync("./Convo/Dataset.json", JSON.stringify(Dataset));
+
+        const Embed = new Discord.MessageEmbed()
+            .setTitle("**SUCCESS**")
+            .setDescription(`Deleted ${sentence} from ${args[1]}-${args[2]}`);
+        message.channel.send(Embed);
+    }
+}
